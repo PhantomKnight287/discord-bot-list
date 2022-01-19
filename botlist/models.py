b4d8d576bbfaa84f70b41916f9edd3cc288ef600
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-19 16:29:39 +0530

    add new fields to models

--- botlist/models.py
+++ botlist/models.py
@@ -9,6 +9,10 @@ class Submission(models.Model):
     owner_name = models.CharField(max_length=35)
     language_used = models.CharField(max_length=20)
     description = models.TextField()
-    site = models.URLField()
+    site = models.URLField(blank=True)
     invite_link = models.URLField()
     bot_id = models.CharField(max_length=18)
+    approved = models.BooleanField()
+    profile_url = models.URLField(blank=True)
+    def __str__(self):
+        return self.bot_name
